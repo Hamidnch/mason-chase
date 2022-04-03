2726fc7ee1c2806dca9a0186d282b345503de9dd
hamidnch <hamidnch2007@gmail.com>
2022-04-03 11:35:53 +0430

    Add project files.

---
+++ Mc2.CrudTest.Presentation/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Mc2.CrudTest.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Mc2.CrudTest.Domain.Generators;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<WeatherForecast> Get()
+        {
+            List<WeatherForecast> response = WeatherForecastGenerator.GenerateWeatherForecast();
+            IPAddress userIp = HttpContext.Request.HttpContext.Connection.RemoteIpAddress;
+            _logger.LogDebug($"[Request from {userIp}: Serving Get() response: {string.Join("\n", response.ToList())}");
+            return response;
+        }
+    }
+}
