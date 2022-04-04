77f342a2f9cc7f4bbe0ff57a4a51e981fe5bec88
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:12:36 +0430

    Added UpdateCustomerCommand to Application Project

---
+++ Core/Mc2.CrudTest.Application/Cqrs/Customers/Commands/UpdateCustomerCommand.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Mc2.CrudTest.Application.Cqrs.Customers.Dtos;
+using Mc2.CrudTest.Application.Cqrs.Customers.Services;
+using MediatR;
+
+namespace Mc2.CrudTest.Application.Cqrs.Customers.Commands
+{
+    public record UpdateCustomerCommand(CustomerDto CustomerDto) : IRequest<CustomerDto>
+    {
+        public record UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand, CustomerDto>
+        {
+            private readonly ICustomerService _customerService;
+
+            public UpdateCustomerCommandHandler(ICustomerService customerService)
+            {
+                _customerService = customerService;
+            }
+
+            public async Task<CustomerDto> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+            {
+                CustomerDto? customerDto = await _customerService.GetByIdAsync(request.CustomerDto.Id);
+
+                customerDto.Firstname = request.CustomerDto.Firstname;
+                customerDto.Lastname = request.CustomerDto.Lastname;
+                customerDto.Email = request.CustomerDto.Email;
+                customerDto.DateOfBirth = request.CustomerDto.DateOfBirth;
+                customerDto.PhoneNumber = request.CustomerDto.PhoneNumber;
+                customerDto.BankAccountNumber = customerDto.BankAccountNumber;
+
+                return await _customerService.UpdateAsync(customerDto);
+            }
+        }
+    }
+}
