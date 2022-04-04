81c3f4d18149a4922b8193eb697faeb44e57ac46
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:10:33 +0430

    Added CustomerDto to Application Project

---
+++ Core/Mc2.CrudTest.Application/Cqrs/Customers/Dtos/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mc2.CrudTest.Application.Cqrs.Customers.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? BankAccountNumber { get; set; }
+    }
+}
