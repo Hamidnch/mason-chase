6de8846646d064da7a562d54eca675b5b5fabda0
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:06:45 +0430

    Mc2.CrudTest.Common Project Created and add Pagination extension to it.

---
+++ Core/Mc2.CrudTest.Common/Pagination/PaginationExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Mc2.CrudTest.Common.Pagination
+{
+    public static class PaginationExtensions
+    {
+        public static IEnumerable<T> ToPaged<T>(this IEnumerable<T> source, int page, int pageSize)
+        {
+            T[] enumerable = source as T[] ?? source.ToArray();
+            return enumerable.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IEnumerable<T> ToPaged<T>(this IEnumerable<T> source, int page, int pageSize, out int rowCount)
+        {
+            T[] enumerable = source as T[] ?? source.ToArray();
+            rowCount = enumerable.Count();
+            return enumerable.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
