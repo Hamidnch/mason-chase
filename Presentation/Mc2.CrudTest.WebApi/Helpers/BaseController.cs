4ef4b963329a681270dcf3a73393ffa7e434596c
hamidnch <hamidnch2007@gmail.com>
2022-04-04 13:17:32 +0430

    Create BaseController for Api controlers

---
+++ Presentation/Mc2.CrudTest.WebApi/Helpers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.WebApi.Helpers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController: ControllerBase
+    {
+
+    }
+}
