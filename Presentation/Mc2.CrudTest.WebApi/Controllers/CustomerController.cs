b9b2b11269204d8366a9cfb636b58c4cd70b0d1b
hamidnch <hamidnch2007@gmail.com>
2022-04-04 13:30:26 +0430

    Created CustomerController and Inject IMediatR to CustomerController constructor

---
+++ Presentation/Mc2.CrudTest.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Mc2.CrudTest.Application.Cqrs.Customers.Commands;
+using Mc2.CrudTest.Application.Cqrs.Customers.Dtos;
+using Mc2.CrudTest.Application.Cqrs.Customers.Queries;
+using Mc2.CrudTest.WebApi.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.WebApi.Controllers
+{
+    public class CustomerController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+ 
+    }
+}
