ff4f851a6523b9d7cedff96871345454f13b96eb
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:11:35 +0430

    Added GetCustomerByIdQuery to Application Project

---
+++ Core/Mc2.CrudTest.Application/Cqrs/Customers/Queries/GetCustomerByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Mc2.CrudTest.Application.Cqrs.Customers.Dtos;
+using Mc2.CrudTest.Application.Cqrs.Customers.Services;
+using MediatR;
+
+namespace Mc2.CrudTest.Application.Cqrs.Customers.Queries
+{
+    public record GetCustomerByIdQuery(int Id) : IRequest<CustomerDto>
+    {
+        public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, CustomerDto>
+        {
+            private readonly ICustomerService _playerService;
+            private readonly IMapper _mapper;
+
+            public GetCustomerByIdQueryHandler(ICustomerService playerService, IMapper mapper)
+            {
+                _playerService = playerService;
+                _mapper = mapper;
+            }
+
+            public async Task<CustomerDto> Handle(GetCustomerByIdQuery query, CancellationToken cancellationToken)
+            {
+                return await _playerService.GetByIdAsync(query.Id);
+            }
+        }
+    }
+}
