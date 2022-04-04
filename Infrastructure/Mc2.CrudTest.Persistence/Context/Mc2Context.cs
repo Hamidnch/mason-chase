42c16c5bab16c2a485ad248e8dc2e4c2b12b93a3
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:14:34 +0430

    Added Mc2Context and Mc2DbContextFactory To Persistence Project

---
+++ Infrastructure/Mc2.CrudTest.Persistence/Context/Mc2Context.cs
@@ -0,0 +1,39 @@
+﻿using Mc2.CrudTest.Application.Interfaces;
+using Mc2.CrudTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace Mc2.CrudTest.Persistence.Context
+{
+    public class Mc2Context : DbContext, IMc2DbContext
+    {
+        #region Ctor
+        public Mc2Context(DbContextOptions<Mc2Context> options) : base(options: options)
+        {
+           
+        }
+        #endregion Ctor
+
+        #region DbSets
+
+        public DbSet<Customer> Customers { get; set; }
+
+        #endregion DbSets
+
+        #region Overrided methods
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        #endregion Overrided methods
+    }
+}
---
+++ Infrastructure/Mc2.CrudTest.Persistence/Context/Mc2DbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Mc2.CrudTest.Persistence.Context
+{
+    public class Mc2DbContextFactory : IDesignTimeDbContextFactory<Mc2Context>
+    {
+        public Mc2Context CreateDbContext(string[] args)
+        {
+            string basePath = Directory.GetCurrentDirectory();
+
+            IConfigurationRoot? configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                //.AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            DbContextOptionsBuilder<Mc2Context> builder = new DbContextOptionsBuilder<Mc2Context>();
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new Mc2Context(builder.Options);
+        }
+    }
+}
