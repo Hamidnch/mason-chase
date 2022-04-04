6b5629155e4f315cd3e0b0758eaf6d6c1b735806
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:20:25 +0430

    Added CustomerProfile for Automapper Config in WebFramework Project

---
+++ Infrastructure/Mc2.CrudTest.WebFramework/AutoMapperConfig/CustomerProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mc2.CrudTest.Application.Cqrs.Customers.Dtos;
+using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.WebFramework.Models;
+
+namespace Mc2.CrudTest.WebFramework.AutoMapperConfig
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+            //CreateMap<CustomerDto, Customer>();
+            CreateMap<Customer, CustomerModel>().ReverseMap();
+            CreateMap<CustomerModel, CustomerDto>().ReverseMap();
+        }
+    }
+}
