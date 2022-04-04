c91a8eb5904e3accf082eecca65fd3fd48179897
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:14:58 +0430

    Added CustomerConfig to mapping folder in Persistence Project

---
+++ Infrastructure/Mc2.CrudTest.Persistence/Mapping/CustomerConfig.cs
@@ -0,0 +1,45 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Persistence.Mapping
+{
+    public class CustomerConfig : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.HasIndex(x => new { x.Firstname, x.Lastname, x.DateOfBirth }).IsUnique().HasFilter(null);
+            builder.HasIndex(x => x.Email).HasDatabaseName("IX_Mc2_Email").IsUnique();
+            builder.Property(x => x.Firstname).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Lastname).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.PhoneNumber).HasMaxLength(15);
+
+            builder.HasData(
+                new Customer()
+                {
+                    Id = 1,
+                    Firstname = "Hamid",
+                    Lastname = "NCH",
+                    Email = "Hamidnch2007@gmail.com",
+                    DateOfBirth = new DateTime(1981, 8, 10),
+                    PhoneNumber = "09124820700",
+                    BankAccountNumber = "123456"
+
+                },
+            new Customer()
+            {
+                Id = 2,
+                Firstname = "Ali",
+                Lastname = "Razavi",
+                Email = "RazaviAli@gmail.com",
+                DateOfBirth = new DateTime(2001, 5, 5),
+                PhoneNumber = "09123526532",
+                BankAccountNumber = "3251388"
+
+            });
+        }
+    }
+}
