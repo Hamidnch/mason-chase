eacb87b1224aeaff1a4bae82594791ecde67f2f7
hamidnch <hamidnch2007@gmail.com>
2022-04-04 13:18:01 +0430

    Added AddApplicationServices to WebApi project(program.cs)

--- Presentation/Mc2.CrudTest.WebApi/Program.cs
+++ Presentation/Mc2.CrudTest.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Mc2.CrudTest.WebFramework.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
