e7904c7e58c30a6fff73f10b6da39c0ffb56d2df
hamidnch <hamidnch2007@gmail.com>
2022-04-04 13:54:45 +0430

    Added LoggingBehavior to Application Project

---
+++ Core/Mc2.CrudTest.Application/Cqrs/Customers/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Mc2.CrudTest.Application.Cqrs.Customers.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse> where TResponse : class where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+                TResponse response = await next();
+                _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occurred!!");
+                _logger.LogError(e, e.Message);
+                return default!;
+            }
+        }
+    }
+}
