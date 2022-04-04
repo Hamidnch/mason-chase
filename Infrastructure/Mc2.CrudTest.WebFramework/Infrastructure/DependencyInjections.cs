24e94cf87e449e5dc1ad6610f295ac5a8f0bd5e5
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:21:59 +0430

    Added Infrastructure fodler in WebFramework Project and added DependencyInjection file to it.

---
+++ Infrastructure/Mc2.CrudTest.WebFramework/Infrastructure/DependencyInjections.cs
@@ -0,0 +1,32 @@
+﻿using Mc2.CrudTest.Application.Cqrs.Customers.Services;
+using Mc2.CrudTest.Application.Interfaces;
+using Mc2.CrudTest.Persistence.Context;
+using Mc2.CrudTest.Persistence.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Mc2.CrudTest.Application.Cqrs.Customers.Queries;
+
+namespace Mc2.CrudTest.WebFramework.Infrastructure
+{
+    public static class DependencyInjections
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            //services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(typeof(GetAllCustomersQuery).GetTypeInfo().Assembly);
+
+            services.AddScoped<IMc2DbContext>(provider => provider.GetService<Mc2Context>()!);
+            services.AddDbContext<Mc2Context>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped(typeof(IMc2Repository<>), typeof(Mc2Repository<>));
+            services.AddScoped<ICustomerService, CustomerService>();
+
+            return services;
+        }
+    }
+}
