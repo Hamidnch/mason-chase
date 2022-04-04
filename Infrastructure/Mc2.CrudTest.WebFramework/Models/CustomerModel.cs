d3d63954e4b893838c3f73c81de3da345668ee3a
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:22:33 +0430

    Added CustomerModel Class in WebFramework Project

---
+++ Infrastructure/Mc2.CrudTest.WebFramework/Models/CustomerModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mc2.CrudTest.WebFramework.Models
+{
+    /// <summary>
+    /// Customer entity
+    /// </summary>
+    public class CustomerModel
+    {
+        /// <summary>
+        /// Customer identifier
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// First name of customer
+        /// </summary>
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string? Firstname { get; set; }
+
+        /// <summary>
+        /// Last name of customer
+        /// </summary>
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string? Lastname { get; set; }
+
+        /// <summary>
+        /// Date of birth of customer
+        /// </summary>
+        [Required(ErrorMessage = "Please enter date of your birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Phone number of customer
+        /// </summary>
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Email of customer
+        /// </summary>
+        [EmailAddress]
+        public string? Email { get; set; }
+        
+        /// <summary>
+        /// Bank account number of customer
+        /// </summary>
+        public string? BankAccountNumber { get; set; }
+    }
+}
