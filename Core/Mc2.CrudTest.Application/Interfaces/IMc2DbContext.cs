adf35a2a09c02d7d85f123d4b5a623fb3e10829e
hamidnch <hamidnch2007@gmail.com>
2022-04-04 12:16:54 +0430

    Added IMc2DbContext To Application Project

---
+++ Core/Mc2.CrudTest.Application/Interfaces/IMc2DbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Application.Interfaces;
+
+public interface IMc2DbContext
+{
+    DbSet<T> Set<T>() where T : class;
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+}
