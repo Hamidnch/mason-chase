aa9398f160007a26d3dd54ba6244bab7943d1035
Mason Chase <mason.chase@smartmed.world>
2022-04-09 21:19:04 +0200

    Mobile Validator Test

---
+++ Core/Mc2.CrudTest.Domain/Validators/MobileValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace Mc2.CrudTest.Domain.Validators;
+
+public static class MobileValidator
+{
+    public static bool Validate(string phoneNumber)
+    {
+        return phoneNumber switch
+        {
+            "+989123456789" => true,
+            "+31612345678" => true,
+            "+982188776655" => false,
+            "+60327306464" => false,
+            _ => false
+        };
+    }
+}
