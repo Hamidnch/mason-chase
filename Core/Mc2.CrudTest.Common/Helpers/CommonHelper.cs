24d9b4992b6167a01af8f29d43764fac9a8be6b8
Hamidnch2007 <Hamidnch2007@gmail.com>
2022-04-10 21:49:27 +0430

    Break IsValidMobileNumber down in 2~3 steps methods

--- Core/Mc2.CrudTest.Common/Helpers/CommonHelper.cs
+++ Core/Mc2.CrudTest.Common/Helpers/CommonHelper.cs
@@ -1,11 +1,48 @@
-﻿using System.Globalization;
+﻿using PhoneNumbers;
+using System.Globalization;
 using System.Text.RegularExpressions;
-using PhoneNumbers;
 
 namespace Mc2.CrudTest.Common.Helpers
 {
     public static class CommonHelper
     {
+        private static readonly PhoneNumberUtil? PhoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+        #region Private Methods
+
+
+        private static PhoneNumber GetStandardPhoneNumber(this string phoneNumber)
+        {
+            PhoneNumber? p = PhoneNumberUtil?.Parse(phoneNumber, null)!;
+            return p;
+        }
+
+        private static bool IsValidMobileType(this PhoneNumber p)
+        {
+            PhoneNumberType? numberType = PhoneNumberUtil?.GetNumberType(p);
+
+            string? phoneNumberType = numberType.ToString();
+
+            return !string.IsNullOrEmpty(phoneNumberType) && phoneNumberType == "MOBILE";
+        }
+        #endregion Private Methods
+
+        public static bool IsValidMobileNumber(this string phoneNumber)
+        {
+            if (PhoneNumberUtil == null)
+            {
+                return false;
+            }
+
+            PhoneNumber? p = phoneNumber.GetStandardPhoneNumber();
+
+            bool isValidNumber = PhoneNumberUtil.IsValidNumber(p);
+
+            bool isValidMobile = p.IsValidMobileType();
+
+            return isValidNumber && isValidMobile;
+        }
+        
         // Examines the domain part of the email and normalizes it.
         public static string DomainMapper(Match match)
         {
@@ -16,23 +53,6 @@ namespace Mc2.CrudTest.Common.Helpers
             string domainName = idn.GetAscii(match.Groups[2].Value);
 
             return match.Groups[1].Value + domainName;
-        }
-
-        public static bool IsValidMobileNumber(this string phoneNumber)
-        {
-            PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.GetInstance();
-            PhoneNumber p = phoneNumberUtil.Parse(phoneNumber, null);
-            bool isMobile = false;
-            bool isValidNumber = phoneNumberUtil.IsValidNumber(p);
-            PhoneNumberType numberType = phoneNumberUtil.GetNumberType(p);
-
-            string phoneNumberType = numberType.ToString();
-
-            if (!string.IsNullOrEmpty(phoneNumberType) && phoneNumberType == "MOBILE")
-            {
-                isMobile = true;
-            }
-            return isValidNumber && isMobile;
         }
     }
 }
